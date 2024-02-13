18c8e9bfbe6da4faec4de62f704267afaacf1108
Juan Luis Rastrollo Guerrero <juanlurasgue06@gmail.com>
2024-02-13 08:45:33 +0100

    Customize the manager

--- webpersonal/webpersonal/urls.py
+++ webpersonal/webpersonal/urls.py
@@ -18,11 +18,16 @@ from django.contrib import admin
 from django.urls import path
 from core import views
 
+from django.conf import settings
+
 urlpatterns = [    
     path('', views.home, name="home"),
     path('admin/', admin.site.urls),
     path('about-me/', views.about, name="about"),
     path('portfolio/', views.portfolio, name="portfolio"),
     path('contact/', views.contact, name="contact"),
-
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
